761ee8e0a9f59f5d724adcb42a1c81e4498645c5
unknown <abhishek.yadav02@sjsu.edu>
2021-11-15 12:33:36 -0800

    Added code after amplify configuration

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,10 +1,26 @@
-import { HttpClient } from '@angular/common/http';
+// import { HttpClient } from '@angular/common/http';
+import { HttpClient,HttpClientModule } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { API } from 'aws-amplify';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
 
-  constructor(httpClient:HttpClient) { }
+  constructor(private httpClient:HttpClient) { }
+
+
+  async createRecipe(body:object){
+    
+      API.post('endlessapi','/recipe', body).then(resp=>{
+        console.log(resp);
+      }).catch(err=>{
+        console.log(err);
+      });
+
+   } 
+
+   
+  
 }
