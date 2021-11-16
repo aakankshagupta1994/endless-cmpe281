e7ef0cb10e6f0adcfade6178f663f5e2ec8529b4
govis99 <govinder.somal@sjsu.edu>
2021-11-15 18:57:01 -0800

    Pushing Receipe Read UI

--- src/app/components/recipe/recipe.component.ts
+++ src/app/components/recipe/recipe.component.ts
@@ -1,16 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { RecipeService } from 'src/app/services/recipe.service';
-
+import { HttpClient,HttpClientModule } from '@angular/common/http';
 @Component({
-  selector: 'app-recipe',
+  selector: 'recipe',
   templateUrl: './recipe.component.html',
   styleUrls: ['./recipe.component.css']
 })
 export class RecipeComponent implements OnInit {
 
-  constructor(recipeService:RecipeService) { }
+  constructor(recipeService:RecipeService, private http:HttpClient) { }
 
+  
   ngOnInit(): void {
+    this.http.get<any>('https://r76zppz36k.execute-api.us-west-2.amazonaws.com/dev/recipes').subscribe((res) => {
+      // console.log(res);
+      // this.ListData = res.Items;
+      // console.log(this.ListData); 
+    });
+
   }
 
 }
