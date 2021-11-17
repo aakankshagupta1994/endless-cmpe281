37813ae82dd14fd5fc16d54f08911c76ea884ea7
Varun Alla <varun.alla@sjsu.edu>
2021-11-17 11:24:50 -0800

    chatbot actions

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -2,6 +2,8 @@ import { Component, EventEmitter, Output } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import {MatSnackBar} from '@angular/material/snack-bar';
+
 
 @Component({
   selector: 'app-nav',
@@ -17,13 +19,18 @@ export class NavComponent {
     );
   @Output() 
   fullfillment = new EventEmitter<any>();
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver,private _snackBar: MatSnackBar) {}
   onBotComplete(event: Event) {
       console.log('chat completed ',event);
       const { data, err } = (event as any).detail;
       if (data) {
             console.log('Chat fulfilled!', JSON.stringify(data));
             this.fullfillment.emit(data);
+            this._snackBar.open(data.slots.MealType, 'dismiss', {
+              horizontalPosition: 'center',
+              verticalPosition: 'bottom',
+              duration:3000
+            });
       }
       if (err) console.error('Chat failed:', err);
   };
