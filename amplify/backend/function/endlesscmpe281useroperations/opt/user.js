06656c5b078fafbd4848669c183941d751c6f001
Varun Alla <varun.alla@sjsu.edu>
2021-11-22 21:14:53 -0800

    user identity service layer

--- amplify/backend/function/endlesscmpe281useroperations/opt/user.js
+++ amplify/backend/function/endlesscmpe281useroperations/opt/user.js
@@ -1,6 +1,11 @@
 var aws = require('aws-sdk');
 let docClient=new aws.DynamoDB.DocumentClient();
 let dynamoDB=new aws.DynamoDB({apiVersion: '2012-08-10'});
+
+let cognitoClient;
+function assignClient(region){
+    cognitoClient = new aws.CognitoIdentityServiceProvider({ region: region });
+}
 async function getItem(params){
     try {
         
@@ -22,6 +27,14 @@ async function addUser(params){
        console.log('adding user error',err);
        return {status:'failure',err:err};
    }
+}
+function getEmailFromCognitoDetails(cognitoUserObj){
+    let email="";
+    for(let userAtt of cognitoUserObj.Attributes){
+        if(userAtt.Name==="email")
+            email=userAtt.Value;
+    }
+    return email;
 }
 async function fetchUser(usertable,username){
     console.log("Fetching User ",usertable,username);
@@ -49,7 +62,54 @@ async function fetchUser(usertable,username){
         return null;
     }
 }
+function fetchUserPoolDetails(request){
+    const authProvider = request.apiGateway.event.requestContext.identity.cognitoAuthenticationProvider;
+    console.log(authProvider);
+    const parts = authProvider.split(':');
+    console.log(parts);
+    const userPoolIdParts = parts[parts.length - 3].split('/');
+    console.log(userPoolIdParts);
+    const userPoolId = userPoolIdParts[userPoolIdParts.length - 1];
+    console.log(userPoolId);
+    const userPoolUserId = parts[parts.length - 1];
+    console.log(userPoolUserId);
+    return {userPoolId,userPoolUserId};
+}
+async function getUserOfAuthenticatedUser(request) {
+    // Get the unique ID given by cognito for this user, it is passed to lambda as part of a large string in event.requestContext.identity.cognitoAuthenticationProvider
+    let result =  fetchUserPoolDetails(request);
+    console.log('user pool details : ',result);
+    let req = {
+        UserPoolId: result.userPoolId,
+        Filter: `sub = "${result.userPoolUserId}"`,
+        Limit: 1
+    }
+    console.log(req);
+    let response = await cognitoClient.listUsers(req).promise();
+    console.log(response.Users);
+    console.log("got user:", response.Users[0])
+    return response.Users[0];
+}
+async function userMiddleWare(req,res,next){
+    console.log('request ',req.apiGateway.event.requestContext);
+    try{
+    let cuser=await getUserOfAuthenticatedUser(req);
+    let email=getEmailFromCognitoDetails(cuser);
+    let user=await fetchUser(process.env.STORAGE_USERS_NAME,email);
+    req.users=user;
+    next();
+    }
+    catch(Err){
+        console.log(Err);
+        res.status(503).json({'Status':'Err'});
+    }
+}
 module.exports={
     fetchUser,
-    addUser
+    addUser,
+    fetchUserPoolDetails,
+    getUserOfAuthenticatedUser,
+    assignClient,
+    getEmailFromCognitoDetails,
+    userMiddleWare
 }
--- amplify/backend/function/userservice/src/app.js
+++ amplify/backend/function/userservice/src/app.js
@@ -1,4 +1,11 @@
-/*
+/* Amplify Params - DO NOT EDIT
+	AUTH_ENDLESSCMPE28139DC59D9_USERPOOLID
+	ENV
+	REGION
+	STORAGE_USERS_ARN
+	STORAGE_USERS_NAME
+	STORAGE_USERS_STREAMARN
+Amplify Params - DO NOT EDIT *//*
 Copyright 2017 - 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
     http://aws.amazon.com/apache2.0/
@@ -12,9 +19,9 @@ const AWS = require('aws-sdk')
 var awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
 var bodyParser = require('body-parser')
 var express = require('express')
-
+const usercode=require('/opt/user');
 AWS.config.update({ region: process.env.TABLE_REGION });
-
+usercode.assignClient(process.env.TABLE_REGION);
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 let tableName = "users";
@@ -32,11 +39,15 @@ const path = "/user";
 const UNAUTH = 'UNAUTH';
 const hashKeyPath = '/:' + partitionKeyName;
 const sortKeyPath = hasSortKey ? '/:' + sortKeyName : '';
+
+
+
+
 // declare a new express app
 var app = express()
 app.use(bodyParser.json())
 app.use(awsServerlessExpressMiddleware.eventContext())
-
+app.use(usercode.userMiddleWare);
 // Enable CORS for all methods
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*")
@@ -54,180 +65,12 @@ const convertUrlType = (param, type) => {
   }
 }
 
-app.get(path,function(req,res){
-  // res.json({username:'varun'});
+app.get(path,async function(req,res){
+  console.log('usercontext ',req.users);
+  return res.json(req.users);
 });
 
 
-/********************************
- * HTTP Get method for list objects *
- ********************************/
-
-app.get(path + hashKeyPath, function(req, res) {
-  var condition = {}
-  condition[partitionKeyName] = {
-    ComparisonOperator: 'EQ'
-  }
-
-  if (userIdPresent && req.apiGateway) {
-    condition[partitionKeyName]['AttributeValueList'] = [req.apiGateway.event.requestContext.identity.cognitoIdentityId || UNAUTH ];
-  } else {
-    try {
-      condition[partitionKeyName]['AttributeValueList'] = [ convertUrlType(req.params[partitionKeyName], partitionKeyType) ];
-    } catch(err) {
-      res.statusCode = 500;
-      res.json({error: 'Wrong column type ' + err});
-    }
-  }
-
-  let queryParams = {
-    TableName: tableName,
-    KeyConditions: condition
-  }
-
-  dynamodb.query(queryParams, (err, data) => {
-    if (err) {
-      res.statusCode = 500;
-      res.json({error: 'Could not load items: ' + err});
-    } else {
-      res.json(data.Items);
-    }
-  });
-});
-
-/*****************************************
- * HTTP Get method for get single object *
- *****************************************/
-
-app.get(path + '/object' + hashKeyPath + sortKeyPath, function(req, res) {
-  var params = {};
-  if (userIdPresent && req.apiGateway) {
-    params[partitionKeyName] = req.apiGateway.event.requestContext.identity.cognitoIdentityId || UNAUTH;
-  } else {
-    params[partitionKeyName] = req.params[partitionKeyName];
-    try {
-      params[partitionKeyName] = convertUrlType(req.params[partitionKeyName], partitionKeyType);
-    } catch(err) {
-      res.statusCode = 500;
-      res.json({error: 'Wrong column type ' + err});
-    }
-  }
-  if (hasSortKey) {
-    try {
-      params[sortKeyName] = convertUrlType(req.params[sortKeyName], sortKeyType);
-    } catch(err) {
-      res.statusCode = 500;
-      res.json({error: 'Wrong column type ' + err});
-    }
-  }
-
-  let getItemParams = {
-    TableName: tableName,
-    Key: params
-  }
-
-  dynamodb.get(getItemParams,(err, data) => {
-    if(err) {
-      res.statusCode = 500;
-      res.json({error: 'Could not load items: ' + err.message});
-    } else {
-      if (data.Item) {
-        res.json(data.Item);
-      } else {
-        res.json(data) ;
-      }
-    }
-  });
-});
-
-
-/************************************
-* HTTP put method for insert object *
-*************************************/
-
-app.put(path, function(req, res) {
-
-  if (userIdPresent) {
-    req.body['userId'] = req.apiGateway.event.requestContext.identity.cognitoIdentityId || UNAUTH;
-  }
-
-  let putItemParams = {
-    TableName: tableName,
-    Item: req.body
-  }
-  dynamodb.put(putItemParams, (err, data) => {
-    if(err) {
-      res.statusCode = 500;
-      res.json({error: err, url: req.url, body: req.body});
-    } else{
-      res.json({success: 'put call succeed!', url: req.url, data: data})
-    }
-  });
-});
-
-/************************************
-* HTTP post method for insert object *
-*************************************/
-
-app.post(path, function(req, res) {
-
-  if (userIdPresent) {
-    req.body['userId'] = req.apiGateway.event.requestContext.identity.cognitoIdentityId || UNAUTH;
-  }
-
-  let putItemParams = {
-    TableName: tableName,
-    Item: req.body
-  }
-  dynamodb.put(putItemParams, (err, data) => {
-    if(err) {
-      res.statusCode = 500;
-      res.json({error: err, url: req.url, body: req.body});
-    } else{
-      res.json({success: 'post call succeed!', url: req.url, data: data})
-    }
-  });
-});
-
-/**************************************
-* HTTP remove method to delete object *
-***************************************/
-
-app.delete(path + '/object' + hashKeyPath + sortKeyPath, function(req, res) {
-  var params = {};
-  if (userIdPresent && req.apiGateway) {
-    params[partitionKeyName] = req.apiGateway.event.requestContext.identity.cognitoIdentityId || UNAUTH;
-  } else {
-    params[partitionKeyName] = req.params[partitionKeyName];
-     try {
-      params[partitionKeyName] = convertUrlType(req.params[partitionKeyName], partitionKeyType);
-    } catch(err) {
-      res.statusCode = 500;
-      res.json({error: 'Wrong column type ' + err});
-    }
-  }
-  if (hasSortKey) {
-    try {
-      params[sortKeyName] = convertUrlType(req.params[sortKeyName], sortKeyType);
-    } catch(err) {
-      res.statusCode = 500;
-      res.json({error: 'Wrong column type ' + err});
-    }
-  }
-
-  let removeItemParams = {
-    TableName: tableName,
-    Key: params
-  }
-  dynamodb.delete(removeItemParams, (err, data)=> {
-    if(err) {
-      res.statusCode = 500;
-      res.json({error: err, url: req.url});
-    } else {
-      res.json({url: req.url, data: data});
-    }
-  });
-});
 app.listen(3000, function() {
     console.log("App started")
 });
